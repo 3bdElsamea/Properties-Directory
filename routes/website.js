1ac76b6ca23a65d00bdce9421bbaff43a1a1f2ca
Ahmed Ramadan <ahmedromio233@gmail.com>
2023-06-09 23:38:29 +0300

    count all tables

--- routes/website.js
+++ routes/website.js
@@ -2,10 +2,13 @@ import express from 'express';
 import authRoute from './website/authRoute.js';
 import { getGeneralSetting } from '../controllers/generalSettingController.js';
 import  ContactUsRoute  from '../routes/website/contactUsRoute.js';
+import couuntTableRoute from '../routes/website/countTableRoute.js';
+
 const router = express.Router();
 
 router.get('/auth', authRoute);
 router.get('/data', getGeneralSetting);
 router.use('/ContactsUs', ContactUsRoute);
+router.use('/countes',couuntTableRoute );
 
 export default router;
