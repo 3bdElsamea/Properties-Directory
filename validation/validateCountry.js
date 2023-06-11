3daa0f54067b40351dc3cf3feca5cdbea3ecdb18
3bdElsamea <asayed4761@gmail.com>
2023-06-11 16:49:40 +0300

    Countries Fixed

--- validation/validateCountry.js
+++ validation/validateCountry.js
@@ -1,16 +1,24 @@
 import joi from 'joi';
 import validationMiddleware from '../middlewares/validationMiddleware.js';
 
-
 const validateCountryCreate = (req, res, next) => {
-  validationMiddleware(req, res, next, joi.object({
+  validationMiddleware(
+    req,
+    res,
+    next,
+    joi.object({
       name: joi.string().min(3).max(255).required(),
     }),
   );
 };
 
+//Update
 const validateCountryUpdate = (req, res, next) => {
-  validationMiddleware(req, res, next, joi.object({
+  validationMiddleware(
+    req,
+    res,
+    next,
+    joi.object({
       name: joi.string().min(3).max(255).allow(),
       active: joi.number().valid(0, 1).allow(),
     }),
