be3c2336757b2f66f4c834c018609479b0ecca5b
Enas <enasgamal.kh@gmail.com>
2023-06-14 02:52:01 +0300

    Website Navbar

---
+++ src/Website/Pages/Properties/Properties-List/Property-List-Filter.js
@@ -0,0 +1,204 @@
+import "./Properties-List.css";
+
+const PropertiesListFilter = () => {
+  return (
+    <>
+      <div className="hsidebar-content">
+        <div className="widget-wrapper">
+          <h6 className="list-title">Find your home</h6>
+          <div className="search_area">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="What are you looking for?"
+            />
+            <label>
+              <span className="flaticon-search"></span>
+            </label>
+          </div>
+        </div>
+        <div className="widget-wrapper">
+          <h6 className="list-title">Listing Status</h6>
+          <div className="radio-element">
+            <div className="form-check d-flex align-items-center mb10">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault4"
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault4">
+                Buy
+              </label>
+            </div>
+            <div className="form-check d-flex align-items-center mb10">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault5"
+                checked="checked"
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault5">
+                Rent
+              </label>
+            </div>
+            <div className="form-check d-flex align-items-center">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="flexRadioDefault"
+                id="flexRadioDefault6"
+              />
+              <label className="form-check-label" htmlFor="flexRadioDefault6">
+                Sold
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="widget-wrapper">
+          <h6 className="list-title">Property Type</h6>
+          <div className="checkbox-style1">
+            <label className="custom_checkbox">
+              Houses
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Apartments
+              <input type="checkbox" checked="checked" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Office
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Villa
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Townhome
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+        </div>
+        <div className="widget-wrapper">
+          <h6 className="list-title">Price Range</h6>
+          <div className="range-slider-style2">
+            <div className="range-wrapper">
+              <div className="mb30 mt35" id="slider"></div>
+              <div className="d-flex align-items-center">
+                <span id="slider-range-value1"></span>
+                <i className="fa-sharp fa-solid fa-minus mx-2 dark-color icon"></i>
+                <span id="slider-range-value2"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="widget-wrapper">
+          <h6 className="list-title">Bedrooms</h6>
+          <div className="d-flex">
+            <div className="selection">
+              <input id="any2" name="beds" type="radio" checked />
+              <label htmlFor="any2">Any</label>
+            </div>
+            <div className="selection">
+              <input id="studio2" name="beds" type="radio" />
+              <label htmlFor="studio2">Studio</label>
+            </div>
+            <div className="selection">
+              <input id="one2" name="beds" type="radio" />
+              <label htmlFor="one2">1+</label>
+            </div>
+            <div className="selection">
+              <input id="two2" name="beds" type="radio" />
+              <label htmlFor="two2">2+</label>
+            </div>
+            <div className="selection">
+              <input id="three2" name="beds" type="radio" />
+              <label htmlFor="three2">3+</label>
+            </div>
+          </div>
+        </div>
+        <div className="widget-wrapper">
+          <h6 className="list-title">Bathrooms</h6>
+          <div className="d-flex">
+            <div className="selection">
+              <input id="any3" name="baths" type="radio" checked />
+              <label htmlFor="any3">Any</label>
+            </div>
+            <div className="selection">
+              <input id="one3" name="baths" type="radio" />
+              <label htmlFor="one3">1+</label>
+            </div>
+            <div className="selection">
+              <input id="two3" name="baths" type="radio" />
+              <label htmlFor="two3">2+</label>
+            </div>
+            <div className="selection">
+              <input id="three3" name="baths" type="radio" />
+              <label htmlFor="three3">3+</label>
+            </div>
+            <div className="selection">
+              <input id="four3" name="baths" type="radio" />
+              <label htmlFor="four3">4+</label>
+            </div>
+          </div>
+        </div>
+        <div className="widget-wrapper">
+          <h6 className="list-title">Amenities</h6>
+          <div className="checkbox-style1">
+            <label className="custom_checkbox">
+              Air Conditioning
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Swimming Pool
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Laundry Room
+              <input type="checkbox" checked="checked" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Gym
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="custom_checkbox">
+              Parking
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+          </div>
+        </div>
+      </div>
+      <div class="widget-wrapper mb20">
+        <div class="btn-area d-grid align-items-center">
+          <button class="ud-btn btn-thm">
+            <span class="flaticon-search align-text-top pr10"></span>Search
+          </button>
+        </div>
+      </div>
+      <div class="reset-area d-flex align-items-center justify-content-between">
+        <a class="reset-button" href="#">
+          <span class="flaticon-turn-back"></span>
+          <u>Reset all filters</u>
+        </a>
+        <a class="reset-button" href="#">
+          <span class="flaticon-favourite"></span>
+          <u>Save Search</u>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default PropertiesListFilter;
