acb336c59bcaf7aca993b9657e6b0c776afaf879
3bdElsamea <asayed4761@gmail.com>
2023-06-19 19:57:29 +0300

    Refactor Notfications & Requests

--- controllers/website/requestController.js
+++ controllers/website/requestController.js
@@ -2,36 +2,84 @@ import catchAsync from '../../utils/catchAsync.js';
 import createReport from '../../utils/report.js';
 import PropertyRequest from '../../models/PropertyRequest.js';
 import Property from '../../models/Property.js';
-import Notification from '../../models/Notification.js';
-import Pusher from 'pusher';
+import sequelize from '../../config/DBConnection.js';
+import AppError from '../../utils/appError.js';
+import createNotification from '../../utils/createNotification.js';
 
-// Pusher Config
-const pusher = new Pusher({
-  appId: '1620529',
-  key: 'fb1c526a7f07ebcc0179',
-  secret: 'ca89ac951f6771fb2686',
-  cluster: 'eu',
-  useTLS: true,
-});
-const createPropertyRequest = catchAsync(async (req, res) => {
+const createPropertyRequest = catchAsync(async (req, res, next) => {
+  const customerId = req.decodedData.customerId;
+  const propertyId = req.params.id;
+
+  //
+  // const query = `
+  //     WITH ins AS (
+  //     INSERT INTO property_requests (customer_id, property_id, status)
+  //     SELECT ${customerId}, ${propertyId}, 'pending'
+  //   WHERE NOT EXISTS (
+  //       SELECT 1
+  //       FROM property_requests
+  //       WHERE customer_id = ${customerId}
+  //             AND property_id = ${propertyId}
+  //             AND status = 'pending'
+  //             )
+  //             RETURNING *
+  //             )
+  //     SELECT *
+  //     FROM ins
+  //     UNION ALL
+  //     SELECT *
+  //     FROM property_requests
+  //     WHERE customer_id = ${customerId}
+  //       AND property_id = ${propertyId}
+  //       AND status = 'pending'
+  //         LIMIT 1
+  // `;
+  //
+  // const propertyRequest = await sequelize.query(query, {
+  //   type: sequelize.QueryTypes.INSERT,
+  //   model: PropertyRequest,
+  //   mapToModel: true,
+  // });
+  //
+  // if (propertyRequest === []) {
+  //   return res
+  //     .status(400)
+  //     .json({ error: 'You already have a pending request for this property.' });
+  // }
+
+  const existingRequest = await PropertyRequest.findOne({
+    where: {
+      customer_id: customerId,
+      property_id: propertyId,
+      status: 'pending',
+    },
+  });
+
+  if (existingRequest) {
+    return next(
+      new AppError(
+        'You already have a pending request for this property.',
+        400,
+      ),
+    );
+  }
+
   const propertyRequest = await PropertyRequest.create({
-    customer_id: req.decodedData.customerId,
-    property_id: req.params.id,
+    customer_id: customerId,
+    property_id: propertyId,
     status: 'pending',
   });
+
   await createReport(
     req,
-    'created a new property request with id ' + propertyRequest.id,
+    'made a new property request with id ' +
+      propertyRequest.id +
+      ' for property with id ' +
+      req.params.id,
   );
-  const notification = await Notification.create({
-    customer_id: req.decodedData.customerId,
-    title: 'New Property Request',
-    message: `New request for Property with id ${propertyRequest.property_id} from Customer with id ${propertyRequest.customer_id}`,
-  });
-  await pusher.trigger('property-request', 'new-request', {
-    title: notification.title,
-    message: notification.message,
-  });
+  const title = 'New Property Request';
+  const message = `New request for Property with id ${propertyRequest.property_id} from Customer with id ${propertyRequest.customer_id}`;
+  await createNotification(customerId, title, message);
 
   res.json(propertyRequest);
 });
