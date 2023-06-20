02b817f8d89b7e4ce14d492a20aaa26a4063d219
3bdElsamea <asayed4761@gmail.com>
2023-06-21 00:15:13 +0300

    Bug Fixes

--- models/chatConversation.js
+++ models/chatConversation.js
@@ -1,33 +1,37 @@
-0import { DataTypes } from 'sequelize';
+import { DataTypes } from 'sequelize';
 import sequelize from '../config/DBConnection.js';
 import Customer from './Customer.js';
 import Employee from './Employee.js';
 
-const ChatConversation = sequelize.define('chat_conversation', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const ChatConversation = sequelize.define(
+  'chat_conversation',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    customer_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'customers',
+        key: 'id',
+      },
+    },
+    employee_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'employees',
+        key: 'id',
+      },
+    },
   },
-  customer_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'customers',
-      key: 'id'
-    }
+  {
+    timestamps: false,
   },
-  employee_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'employees',
-      key: 'id'
-    }
-  }
-}, {
-  timestamps: false
-});
+);
 
 ChatConversation.belongsTo(Customer, { foreignKey: 'customer_id' });
 ChatConversation.belongsTo(Employee, { foreignKey: 'employee_id' });
