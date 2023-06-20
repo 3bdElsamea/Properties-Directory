33ea89b9f39df2b8297ef0ba18ea994b73645bb9
3bdElsamea <asayed4761@gmail.com>
2023-06-20 23:43:02 +0300

    Rent/Buy Handled

--- controllers/dashboard/categoryController.js
+++ controllers/dashboard/categoryController.js
@@ -5,12 +5,22 @@ import ApiFeatures from '../../utils/apiFeatures.js';
 import createReport from '../../utils/report.js';
 
 const getAllCategories = catchAsync(async (req, res, next) => {
-  const categories = await new ApiFeatures(Category, req.query).get();
+  // const categories = await new ApiFeatures(Category, req.query).get();
+  // find only the categories with id 24 and 25
+  const categories = await Category.findAll({
+    where: {
+      id: [24, 25],
+    },
+  });
   res.json(categories);
 });
 
 const getCategoryById = catchAsync(async (req, res, next) => {
   const { id } = req.params;
+  // if id not 24 or 25 throw error
+  if (id !== '24' && id !== '25') {
+    return next(new AppError(`Category with this id ${id} not found`, 404));
+  }
   const category = await Category.findByPk(id);
   if (!category) {
     return next(new AppError(`Category with this id ${id} not found`, 404));
@@ -18,51 +28,51 @@ const getCategoryById = catchAsync(async (req, res, next) => {
   res.json(category);
 });
 
-const createCategory = catchAsync(async (req, res) => {
-  const { name } = req.body;
-  const category = await Category.create({
-    name,
-  });
-  await createReport(req, 'create new category name: ' + name);
-  res.status(201).json({ data: category });
-});
+// const createCategory = catchAsync(async (req, res) => {
+//   const { name } = req.body;
+//   const category = await Category.create({
+//     name,
+//   });
+//   await createReport(req, 'create new category name: ' + name);
+//   res.status(201).json({ data: category });
+// });
 
-const updateCategory = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const category = await Category.findByPk(id);
-  if (!category) {
-    return next(new AppError(`Category with this id ${id} not found`, 404));
-  }
-  await category.update({
-    ...req.body,
-  });
+// const updateCategory = catchAsync(async (req, res, next) => {
+//   const { id } = req.params;
+//   const category = await Category.findByPk(id);
+//   if (!category) {
+//     return next(new AppError(`Category with this id ${id} not found`, 404));
+//   }
+//   await category.update({
+//     ...req.body,
+//   });
 
-  await createReport(req, 'updated category named : ' + category.name);
+//   await createReport(req, 'updated category named : ' + category.name);
+//
+//   res.json(category);
+//   next();
+// });
 
-  res.json(category);
-  next();
-});
-
-const inactiveCategory = catchAsync(async (req, res, next) => {
-  const { id } = req.params;
-  const category = await Category.findByPk(id);
-
-  if (!category) {
-    return next(new AppError(`Category with id ${id} not found`, 404));
-  }
-
-  await category.toggleActive();
-
-  if (category.active)
-    await createReport(req, 'activated category named : ' + category.name);
-  else await createReport(req, 'deactivated category named : ' + category.name);
-  res.json({ message: 'Category updated', category });
-});
+// const inactiveCategory = catchAsync(async (req, res, next) => {
+//   const { id } = req.params;
+//   const category = await Category.findByPk(id);
+//
+//   if (!category) {
+//     return next(new AppError(`Category with id ${id} not found`, 404));
+//   }
+//
+//   await category.toggleActive();
+//
+//   if (category.active)
+//     await createReport(req, 'activated category named : ' + category.name);
+//   else await createReport(req, 'deactivated category named : ' + category.name);
+//   res.json({ message: 'Category updated', category });
+// });
 
 export {
   getAllCategories,
   getCategoryById,
-  updateCategory,
-  inactiveCategory,
-  createCategory,
+  // updateCategory,
+  // inactiveCategory,
+  // createCategory,
 };
--- routes/dashboard/categoryRoute.js
+++ routes/dashboard/categoryRoute.js
@@ -2,25 +2,26 @@ import express from 'express';
 
 import {
   getAllCategories,
-  createCategory,
+  // createCategory,
   getCategoryById,
-  updateCategory,
-  inactiveCategory,
+  // updateCategory,
+  // inactiveCategory,
 } from '../../controllers/dashboard/categoryController.js';
 import idParmaMiddleware from '../../middlewares/idParmaMiddleware.js';
-import { validationCreateCategory, validationUpdateCategory } from '../../validation/validateCategory.js';
+import {
+  validationCreateCategory,
+  validationUpdateCategory,
+} from '../../validation/validateCategory.js';
 
 const router = express.Router();
 
-router.route('/').get(getAllCategories).post(createCategory);
+router.route('/').get(getAllCategories);
+// .post(createCategory);
 
-router
-  .route('/:id')
-  .all(idParmaMiddleware)
-  .get(getCategoryById)
-  .post(validationCreateCategory, createCategory)
-  .patch(validationUpdateCategory, updateCategory)
+router.route('/:id').all(idParmaMiddleware).get(getCategoryById);
+// .post(validationCreateCategory, createCategory)
+// .patch(validationUpdateCategory, updateCategory)
 
-router.route('/:id/toggle-active').all(idParmaMiddleware).patch(inactiveCategory);
+// router.route('/:id/toggle-active').all(idParmaMiddleware).patch(inactiveCategory);
 
 export default router;
